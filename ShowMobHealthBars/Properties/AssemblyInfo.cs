f42fa8a3ba62211b4563cd59dd451afc1a720035
alpeerkaraca <alpeerkaraca@gmail.com>
2024-09-21 00:25:06 +0300

    Updated and TR lang. support added.

---
+++ ShowMobHealthBars/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("7fec4c11-4c49-4e0f-b7e2-fa999e17c4a4")]
